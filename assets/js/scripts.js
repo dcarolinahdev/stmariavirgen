ab5710da33f3f18994083171d968468281a333a6
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2025-06-27 10:45:46 -0500

    Improving styles for homepage v1 (#1)

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+let homepage_title = document.getElementById("homepage-title");
+let homepage_subt = document.getElementById("homepage-subt");
+
+let homepage_title_val = "Reina del Santísimo Rosario";
+let homepage_subt_val = "Ruega por nosotros";
+
+homepage_title.innerHTML = homepage_title_val;
+homepage_subt.innerHTML = homepage_subt_val;
+
