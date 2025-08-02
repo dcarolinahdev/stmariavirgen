98a0d27a457260f8a07411f05b8e537ced0f0e70
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2025-08-02 11:38:44 -0500

    Calendar Section V1 (#2)
    * Metadata v2
    
    * Styles for calendar section v1
    
    * Styles for calendar section v2
    
    * Styles for calendar section v3
    
    * Fix z index
    
    * Events for timeline in calendar section

---
+++ assets/js/events.js
@@ -0,0 +1,152 @@
+const events = [
+    {   "date": '01/01/2025',
+        "name": "Santa María Madre de Dios",
+        "left": false,
+        "type": "solemnity"
+    },
+    {   "date": '01/05/2025',
+        "name": "La Epifanía del Señor",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '01/12/2025',
+        "name": "El bautismo del Señor",
+        "left": true,
+        "type": "party"
+    },
+    {   "date": '02/02/2025',
+        "name": "Presentación del Señor",
+        "left": true,
+        "type": "party"
+    },
+    {   "date": '02/11/2025',
+        "name": "Nuestra Señora de Lourdes",
+        "left": false,
+        "type": "memoryf"
+    },
+    {   "date": '02/22/2025',
+        "name": "Cátedra de San Pedro",
+        "left": true,
+        "type": "party"
+    },
+    {   "date": '03/19/2025',
+        "name": "San José",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '03/25/2025',
+        "name": "La Anunciación del Señor",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '05/13/2025',
+        "name": "Nuestra Señora de Fátima",
+        "left": false,
+        "type": "memoryf"
+    },
+    {   "date": '05/31/2025',
+        "name": "La Visitación de la Bienaventurada Virgen María",
+        "left": false,
+        "type": "party"
+    },
+    {   "date": '06/01/2025',
+        "name": "La Ascención del Señor",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '06/08/2025',
+        "name": "Pentecostés",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '06/15/2025',
+        "name": "La Santísima Trinidad",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '06/22/2025',
+        "name": "El cuerpo y La sangre santísimos de Cristo",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '06/24/2025',
+        "name": "El nacimiento de San Juan Bautista",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '06/27/2025',
+        "name": "El Sagrado Corazón de Jesús",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '06/29/2025',
+        "name": "Santos Pedro y Pablo Apóstoles",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '07/09/2025',
+        "name": "Nuestra Señora del Rosario de Chiquinquirá",
+        "left": false,
+        "type": "party"
+    },
+    {   "date": '07/16/2025',
+        "name": "Nuestra Señora del Carmen",
+        "left": false,
+        "type": "memoryf"
+    },
+    {   "date": '08/06/2025',
+        "name": "La Transfiguración del Señor",
+        "left": true,
+        "type": "party"
+    },
+    {   "date": '08/15/2025',
+        "name": "La Asunción de la Santísima Virgen María",
+        "left": false,
+        "type": "solemnity"
+    },
+    {   "date": '08/29/2025',
+        "name": "Martirio de San Juan Bautista",
+        "left": true,
+        "type": "memoryf"
+    },
+    {   "date": '09/08/2025',
+        "name": "La Natividad de la Bienaventurada Virgen María",
+        "left": false,
+        "type": "party"
+    },
+    {   "date": '09/14/2025',
+        "name": "Exaltación de la Santa Cruz",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '09/15/2025',
+        "name": "Nuestra Señora de los Dolores",
+        "left": false,
+        "type": "memoryf"
+    },
+    {   "date": '11/01/2025',
+        "name": "Todos los Santos",
+        "left": true,
+        "type": "solemnity"
+    },
+    {   "date": '11/21/2025',
+        "name": "Presentación de la Santísima Virgen María",
+        "left": false,
+        "type": "party"
+    },
+    {   "date": '12/08/2025',
+        "name": "La Inmaculada Concepción de la Bienaventurada Virgen María",
+        "left": false,
+        "type": "solemnity"
+    },
+    {   "date": '12/12/2025',
+        "name": "Nuestra Señora de Guadalupe",
+        "left": false,
+        "type": "party"
+    },
+    {   "date": '12/25/2025',
+        "name": "El Nacimiento del Señor",
+        "left": true,
+        "type": "solemnity"
+    }
+];
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,27 @@ let homepage_subt_val = "Ruega por nosotros";
 homepage_title.innerHTML = homepage_title_val;
 homepage_subt.innerHTML = homepage_subt_val;
 
+let timelinediv = document.getElementById('timeline');
+
+events.forEach(item => {
+    // create new div
+    const newDiv = document.createElement("div");
+    // and give it some content
+    newDiv.classList.add('event');
+    newDiv.classList.add(item.type);
+    (item.left) ? newDiv.classList.add('left') : newDiv.classList.add('right');
+    let eventdate = new Date(item.date);
+    let eventdatef = eventdate.toLocaleDateString("es-CO",
+        {   weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        }
+    );
+    newDiv.innerHTML = `
+        <p>${eventdatef}</p>
+        <h4>${item.name}</h4>
+    `;
+    
+    timelinediv.appendChild(newDiv);
+});
